4ea0a28b8293c94470d17e292ea07826f4806ed4
brunogeek9 <brunogeek9@gmail.com>
2020-05-12 19:26:56 -0300

    implementada a listagem de posts.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
+
+
+def home(request):
+    posts = Post.objects.all()
+    # for item in posts:
+    #     print(item.content)
+    return render(request, 'home.html', {'posts': posts})
